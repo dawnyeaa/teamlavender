91f3f3e72940494dd4afbfc1d3b9651d5f65db1b
todd <contact@toddfanning.com>
2023-07-20 13:48:15 +1000

    we got character customisation with models going

--- Assets/Personal/Todd/CustomiseCharacter.cs
+++ Assets/Personal/Todd/CustomiseCharacter.cs
@@ -4,28 +4,20 @@ using System.Linq;
 using UnityEngine;
 
 public class CustomiseCharacter : MonoBehaviour {
-    private GameObject body;
-    private List<CustomiseSlot> slots;
+  private GameObject body;
+  private List<CustomiseSlot> slots;
 
-    void Start() {
-        slots = transform.GetComponents<CustomiseSlot>().ToList();
-        body = transform.Find("body").gameObject;
-        for (int i = 0; i < transform.childCount; ++i) {
-            GameObject child = transform.GetChild(i).gameObject;
-            if (child != body) {
-                Destroy(child);
-            }
-        }
-
-        foreach (CustomiseSlot slot in slots) {
-            List<GameObject> options = slot.GetOptionMeshes();
-            for (int i = 0; i < options.Count; ++i) {
-                slot.SetOptionInstance(Instantiate(options[i], transform), i);
-            }
-        }
+  void Awake() {
+    body = transform.Find("body").gameObject;
+    for (int i = 0; i < transform.childCount; ++i) {
+      GameObject child = transform.GetChild(i).gameObject;
+      if (child != body) {
+        Destroy(child);
+      }
     }
+  }
 
-    void Update() {
-        
-    }
+  void Update() {
+    
+  }
 }
--- Assets/Personal/Todd/CustomiseSlot.cs
+++ Assets/Personal/Todd/CustomiseSlot.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomiseSlot : MonoBehaviour {
-    public string slotName;
-    public List<GameObject> options;
-    public int selected;
+  public string slotName;
 
-    void Start() {
-        
-    }
+  [SerializeField]
+  private List<GameObject> optionMeshes;
+  private List<GameObject> options;
+  public int selected;
+  public readonly int defaultOption;
 
-    void Update() {
-        
+  void Awake() {
+    options = new List<GameObject>(optionMeshes.Count);
+    selected = defaultOption;
+  }
+
+  void Start() {
+    InstantiateOptions();
+    SetActiveStates();
+  }
+
+  private void InstantiateOptions() {
+    for (int i = 0; i < optionMeshes.Count; ++i) {
+      options.Insert(i, Instantiate(optionMeshes[i], transform));
     }
+  }
+
+  private void SetActiveStates() {
+    for (int i = 0; i < options.Count; ++i) {
+      options[i].SetActive(i == selected);
+    }
+  }
+
+  public void SelectNextOption() {
+    selected = selected == (options.Count-1) ? 0 : selected + 1;
+    SetActiveStates();
+  }
+
+  public void SelectPreviousOption() {
+    selected = selected == 0 ? options.Count-1 : selected - 1;
+    SetActiveStates();
+  }
 }
