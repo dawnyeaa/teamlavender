ffe1940d770afe38620ca06dedd1e2f2680a49fb
todd <hello.tfanning@gmail.com>
2023-08-01 11:56:42 +1000

    had to split the readonly property stuff into sep files

--- Assets/Personal/Todd/CharController v3/Skateboard3StateMachine.cs
+++ Assets/Personal/Todd/CharController v3/Skateboard3StateMachine.cs
@@ -1,31 +1,4 @@
 using UnityEngine;
-using UnityEditor;
-
-#region ReadonlyAttribute
-public class ReadOnlyAttribute : PropertyAttribute
-{
-
-}
-
-[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
-public class ReadOnlyDrawer : PropertyDrawer
-{
-    public override float GetPropertyHeight(SerializedProperty property,
-                                            GUIContent label)
-    {
-        return EditorGUI.GetPropertyHeight(property, label, true);
-    }
-
-    public override void OnGUI(Rect position,
-                               SerializedProperty property,
-                               GUIContent label)
-    {
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
-    }
-}
-#endregion
 
 [RequireComponent(typeof(InputController))]
 [RequireComponent(typeof(WheelController))]
