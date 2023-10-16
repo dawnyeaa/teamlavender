0094a39a83993890024f79cb5f5175dd1b4d3fec
Harry Bosch/Laptop <harrybosch03@gmail.com>
2023-10-16 18:51:15 +1100

    Updated Collision Processor to only use the players forward speed.

--- Assets/Scripts/CharacterController/SkateboardCollisionProcessor.cs
+++ Assets/Scripts/CharacterController/SkateboardCollisionProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -10,22 +11,30 @@ namespace CharacterController
     [System.Serializable]
     public class SkateboardCollisionProcessor
     {
-        [SerializeField] private float threshold;
+        [SerializeField] private bool enabled = true;
+        [SerializeField] private float speedThresholdKmpH = 30.0f;
         [SerializeField] [ReadOnly] private float peak;
+        [SerializeField] [ReadOnly] private float speed;
 
         private Vector3 lastVelocity;
 
         public void FixedUpdate(SkateboardStateMachine target)
         {
+            if (!enabled) return;
+            
             var rb = target.MainRB;
             var velocity = rb.velocity;
 
-            var diff = (velocity - lastVelocity) / Time.deltaTime;
-            var fac = diff.magnitude - threshold;
+            var deltaVelocity = velocity - lastVelocity;
+            var deltaSpeedKmpH = Mathf.Abs(Vector3.Dot(rb.transform.forward, deltaVelocity * 3.6f));
 
-            if (fac > 0.0f) target.Die(lastVelocity);
+            if (deltaSpeedKmpH > speedThresholdKmpH)
+            {
+                target.Die(lastVelocity);
+            }
 
-            peak = Mathf.Max(peak, fac);
+            peak = Mathf.Max(peak, deltaSpeedKmpH);
+            speed = velocity.magnitude * 3.6f;
             lastVelocity = velocity;
         }
     }
