07411b2a3bd5b67263a7fbafd7e61ba94f0dfeda
todd <hello.tfanning@gmail.com>
2023-11-03 14:40:06 +1100

    still integrating the command list menu, gonna take some finnicking but this should fix runtime errors

--- Assets/Scripts/UI/MenuCanceller.cs
+++ Assets/Scripts/UI/MenuCanceller.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 public class MenuCanceller : MonoBehaviour, ICancelHandler {
   public UnityEvent CancelEvent;
   public void OnCancel(BaseEventData eventData) {
+    Debug.Log("cancelling");
     CancelEvent.Invoke();
   }
 }
